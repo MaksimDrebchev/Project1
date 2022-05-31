0e0371079f1c8f48426a544460339f9fe2cbadea
Maksim Drebchev <maksim.drebchev@grafixoft.com>
2022-05-31 15:34:25 +0300

    Adding Project 1 to GIT

---
+++ ConsoleApp1/ConsoleApp1/School.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class School
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Student> Students { get; private set; }
+
+        public School(string name, string address)
+        {
+            Name = name;
+            Address = address;
+            Students = new List<Student>();
+        }
+
+        public void AddStudent(Student student)
+        {
+            Students.Add(student);
+        }
+
+        public void AddStudents(List<Student> students) => Students.AddRange(students);
+
+        public void RemoveStudent(int id)
+        {
+            foreach (Student student in Students)
+            {
+                if (student.Id == id)
+                {
+                    Students.Remove(student);
+                }
+            }
+        }
+
+        public List<Student> GetExcellentStudents()
+        {
+            List<Student> otlichnici = new List<Student>();
+            foreach (var student in Students)
+            {
+                int lowerScore = 6;
+                foreach (int item in student.belejnik.Values)
+                {
+                    if (item < 6)
+                    {
+                        lowerScore = item;
+                    }
+                }
+                if (lowerScore == 6)
+                {
+                    student.isExcellent = true;
+                    otlichnici.Add(student);
+                }
+            }
+            return otlichnici;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Id { get; private set; }
+        private int _age;
+        public Dictionary<string, int> belejnik = new Dictionary<string, int>();
+        public bool isExcellent = false;
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value >= 7)
+                {
+                    _age = value;
+                }  
+                else
+                {
+                    throw new ArgumentException("Student age is less than 7");
+                }    
+            }
+        }
+
+        public Student (string name, int age, Dictionary<string, int> marks)
+        {
+            Name = name;
+            Age = age;
+            belejnik = marks;
+            Random random = new Random();
+            int randomNumber = random.Next();
+            Id = randomNumber;
+        }
+
+        public void Speak() => Console.WriteLine(Message);
+
+        private int YearsLeft => 18 - Age;
+        private string Message
+        {
+            get
+            {
+                string message;
+                if (Age < 18)
+                {
+                    message = $"Hello I'm {Name} and I've got {YearsLeft} years to graduate.";
+                }
+                else
+                {
+                    message = $"Hello I’m {Name} and I'll graduate this year.";
+                }
+                return message;
+            }
+        }
+    }
+}
